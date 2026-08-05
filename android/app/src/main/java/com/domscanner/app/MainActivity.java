12f1a1278eb8eb9a11beec457602bd15a11e2947
DOMScanner Admin <admin@domscanner.com>
2026-08-05 17:37:09 +0530

    Feature: Add complete Android Studio project structure (build.gradle, AndroidManifest, MainActivity)

---
+++ android/app/src/main/java/com/domscanner/app/MainActivity.java
@@ -0,0 +1,12 @@
+package com.domscanner.app;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(NetworkScannerPlugin.class);
+    }
+}
